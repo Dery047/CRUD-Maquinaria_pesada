d792468769abfbd9ebfc991d5c2ebdd03ee0ca58
Santiago <vergarasantiago049@gmail.com>
2025-06-27 23:09:42 -0500

    fixing minor bugs in models.py, documentation in serializers.py

--- vehicles_app/models.py
+++ vehicles_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+#creating Vehiculo model and its fields.
 class Vehiculo(models.Model):
     marca = models.CharField(max_length=150)
     numero_registration = models.CharField(max_length=100, unique=True)
@@ -10,3 +11,8 @@ class Vehiculo(models.Model):
     def __str__(self):
         return f"{self.marca} - {self.numero_registration}"
 
+# Nota: este programa solo permite el registro de los vehiculos y que puedan ser almacenados en una database
+# A futuro se le agregarán otros metodos como la eliminación de vehiculos y otras características segun sea necesario.
+
+# Note: This program currently only allows vehicle registration and stores them in a database.
+# Additional features like vehicle deletion and other functionalities will be added in the future as needed.
--- vehicles_app/serializers.py
+++ vehicles_app/serializers.py
@@ -4,4 +4,4 @@ from .models import Vehiculo
 class VehiculoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Vehiculo
-        fields = '__all__'
+        fields = '__all__' #includes all fields from the app model
